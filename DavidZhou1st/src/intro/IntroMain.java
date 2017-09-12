231950e0bb5682eb320c5938686a47ec13f243ab
BT_1N3_28 <dzhou7032@bths.edu>
2017-09-12 11:46:03 -0400

    OH yeaaaa

--- DavidZhou1st/src/intro/IntroMain.java
+++ DavidZhou1st/src/intro/IntroMain.java
@@ -4,7 +4,8 @@ package intro;//this is a package declaration
 public class IntroMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World!");
 
 	}
 
