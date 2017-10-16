760a276b91f14faa582d4a30ea2f9cb2474e1a4a
BT_1N3_28 <BT_1N3_28@BT_1N3_28.bktech.local>
2017-10-16 11:43:48 -0400

    asfd

---
+++ DavidZhou1st/src/arrays/ArrayMain.java
@@ -0,0 +1,88 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class ArrayMain {
+	private int[] testArray;
+	private int[]intRay;
+	
+	public ArrayMain() {
+		intRay = new int[10];
+		//populate(intRay);
+		//checkOccurences(intRay,2,12);
+		populate1ToN(intRay);
+		swap(intRay,0,1);
+		shuffle(intRay);
+		//Arrays is a Utility class included in Java for formatting output
+		System.out.println(Arrays.toString(intRay));
+	}
+	private void shuffle(int[] intRay2) {
+		for (int i=0; i<intRay2.length;i++) {
+			swap(intRay2,(int)(Math.random()*intRay2.length),(int)(Math.random()*intRay2.length));
+		}
+	}
+	private void swap(int[] intRay2, int i, int j) {
+		int placeholder = intRay2[i];
+		intRay2[i]=intRay2[j];
+		intRay2[j]=placeholder;
+	}
+	private void populate1ToN(int[] intRay) {
+		for(int i=0;i<intRay.length; i++) {
+			intRay[i] = i+1;
+		}
+	}
+	private void checkOccurences(int[] arr, int start, int end) {
+		int[] counter = new int[end-start+1];
+		for (int value:arr) {
+			counter[value-start]++;
+		}
+		for (int i=0;i<counter.length;i++) {
+			System.out.println("A "+(start+i)+" was rolled "+counter[i]+" times.");
+		}
+	}
+	private void populate(int[] intRay) {
+		for(int i = 0;i<intRay.length;i++) {
+			intRay[i]= diceRoll(2);
+		}
+	}
+	public void notes() {
+		//1.collection of primitives or objects
+				//SPECIAL NOTE: This is the ONLY collection of primitives
+				//2.size cannot be modified
+				//3.Elements of an array are REFERENCES to objects.
+				//in other words, changing an element of an array changes
+				//the REFERENCE, not the object(more on this later)
+				
+				//There are two types of constructors, the first we've seen already:
+				int[] firstType = {3,14,-9,10};
+				//this constructor can only be used at the declaration. Error:
+				//firstType = {12,13,14,15};
+				
+				//the 2nd type:
+				testArray = new int[20];
+				//SPECIAL NOTE: for primitive arrays, when they are instantiated,
+				//they are automatically populated with 0s. This is not the case with Object arrays,
+				//which are populated with 'null'
+				
+				//standard 'for' loop
+				for(int i = 0;i<testArray.length;i++) {
+					System.out.println(testArray[i]);
+				}
+				//for each loop(useful only when you don't need to keep track of an index
+				for (int value:testArray) {
+					//"for each int in testArray..."
+					System.out.println(value);
+				}
+	}
+	public static void main(String[] args) {
+		ArrayMain arrayPractice = new ArrayMain();
+	}
+	
+	public int diceRoll(int numberOfDice) {
+	int roll = 0;
+	for (int i = 0; i<numberOfDice; i++) {
+		roll += 1+(int)(Math.random()*6);
+	}
+	return roll;
+	}
+}
