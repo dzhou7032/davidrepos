11fdabfc4bd59572cf76a3e3c649775c6fc9497a
BT_1N3_28 <BT_1N3_28@BT_1N3_28.bktech.local>
2017-10-17 11:45:28 -0400

    ewr

--- DavidZhou1st/src/arrays/ArrayMain.java
+++ DavidZhou1st/src/arrays/ArrayMain.java
@@ -6,15 +6,77 @@ public class ArrayMain {
 	private int[] testArray;
 	private int[]intRay;
 	
+	
 	public ArrayMain() {
-		intRay = new int[10];
+		int[] TestArr1 = {1,2,3,6,7,8,9,10,11,45,46,47};
+		intRay = new int[20];
 		//populate(intRay);
 		//checkOccurences(intRay,2,12);
 		populate1ToN(intRay);
-		swap(intRay,0,1);
-		shuffle(intRay);
+		//swap(intRay,0,1);
+		//shuffle(intRay);
+		reverseOrder(intRay);
+		countLessThan(intRay, 5);
+		//frontToBack(intRay);
+		longestConsecutiveSequence(TestArr1);
 		//Arrays is a Utility class included in Java for formatting output
-		System.out.println(Arrays.toString(intRay));
+		System.out.println(Arrays.toString(TestArr1));
+	}
+	private int countLessThan(int[] intRay2, int num) {
+		int counter = 0;
+		for (int i=0;i<intRay2.length;i++) {
+			if (intRay2[i]<num) {
+				counter++;
+			}
+		}
+		return counter;
+	}
+	public void cycleThrough(int[] arr, int n) {
+		for (int i = 0;i<n;i++) {
+			frontToBack(arr);
+		}
+	}
+	public void frontToBack(int[] arr) {
+		int placeholder = arr[0];
+		for (int i=0;i<arr.length-1;i++) {
+			arr[i] = arr[i+1];
+		}
+		arr[arr.length-1]=placeholder;
+	}
+	private void reverseOrder(int[] intRay2) {
+		int[] newArray = new int[intRay2.length];
+		for (int i=0; i<intRay2.length;i++) {
+			newArray[i]=intRay2[i];
+		}
+		for (int i=0; i<newArray.length/2; i++) {
+			swap(newArray, i, newArray.length-1-i);
+		}	
+	}
+	public int longestConsecutiveSequence(int[] arr) {
+	int counter=0;
+	int max=0;
+	int posi = 1;
+		for (int i=0;i<arr.length-1; i++) {
+		if (arr[i]+1==arr[i+1]) {
+			counter++;
+		}
+		else {
+			if(counter>max) {
+			max = counter;
+			posi = i+1;
+			counter = 0;
+			}
+			else {
+				counter = 0;
+			}
+		}
+	}
+		if (counter>max) {
+			max=counter;
+		}
+		System.out.print(max+1);
+		System.out.print(posi);
+		return max+1;
 	}
 	private void shuffle(int[] intRay2) {
 		for (int i=0; i<intRay2.length;i++) {
